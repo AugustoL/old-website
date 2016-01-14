a41ffa0aee7dfce96d4b4aaed5ffc5ced3650b82
AugustoL <augsuto.lemble@gmail.com>
2016-01-14 12:41:26 -0300

    Update gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	logLevel : 'debug', // none, normal, debug
-	dbURI : 'mongodb://user:15ssc51s65e1et65svt4k5u4@127.0.0.1:27017/augustolemble', //URI to use to connect to db
-	spotifyCredentials : {
-        clientId : '52448dda6b344eac852886a2bfa52b93',
-        clientSecret : '6d04e343e08d4bfb8f7e7abdd00a6135',
-        myUser : '11120104543'
-    }
-};
